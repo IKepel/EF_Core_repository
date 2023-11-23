5bc399fc73f0d01881f58ba3edda9d1fb8c152c2
Kostya Popov <kepel2823@gmail.com>
2023-11-23 16:01:25 +0200

    Add project files.

---
+++ EF_Core_repository/MovieLibrary.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_repository
+{
+    public class MovieLibrary : IEnumerable
+    {
+        private readonly AppDbContext _dbContext;
+
+        public MovieLibrary(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var movie in _dbContext.Movies.Where(m => IsNightTime() || !m.IsAdult))
+            {
+                yield return movie.Title;
+            }
+        }
+
+        private bool IsNightTime() => DateTime.Now.Hour >= 23 || DateTime.Now.Hour < 7;
+    }
+}
---
+++ EF_Core_repository/Program.cs
@@ -0,0 +1,24 @@
+﻿using EF_Core_repository;
+using Microsoft.EntityFrameworkCore;
+
+using var myDb = new AppDbContext();
+
+if (!myDb.Movies.Any())
+{
+    myDb.Movies.AddRange(
+        new Movie { Title = "Harry Potter", IsAdult = false },
+        new Movie { Title = "The Lord of the Rings", IsAdult = false },
+        new Movie { Title = "Transformers", IsAdult = false },
+        new Movie { Title = "Gladiator", IsAdult = true },
+        new Movie { Title = "Piranha 3D", IsAdult = true },
+        new Movie { Title = "Room in Rome", IsAdult = true }
+    );
+    myDb.SaveChanges();
+}
+
+var movieLibrary = new MovieLibrary(myDb);
+
+foreach (var movie in movieLibrary)
+{
+    Console.WriteLine(movie);
+}
